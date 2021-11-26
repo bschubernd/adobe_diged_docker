8d3fc2b6b4a9f6d393a6bd476e11689e27a4cd78
anonymous <x@example.com>
2021-11-26 00:00:00 +0000

    Install pycrypto unattended

--- container/install.sh
+++ container/install.sh
@@ -10,7 +10,10 @@ msiexec /i wine-mono-7.0.0-x86.msi
 winetricks -q python27
 winetricks -q autohotkey
 
-wine pycrypto-2.6.1.win32-py2.7.exe
+7z x pycrypto-2.6.1.win32-py2.7.exe PLATLIB/Crypto
+mv PLATLIB/Crypto ".wine/drive_c/Python27/Lib/site-packages"
+rmdir PLATLIB
+
 7z x -aos -o".wine/drive_c/Program Files/Adobe/Adobe Digital Editions 2.0/" ADE_2.0_Installer.exe
 mkdir -p "My Digital Editions"
 
